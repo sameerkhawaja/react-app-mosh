0d51e166debcddb66b97c5d04afbbda79347808c
sameerkhawaja <sameerkhawaja01@gmail.com>
2023-07-25 14:12:22 -0400

    use effect demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,22 @@
-import { useState } from "react";
-import ExpenseList from "./components/ExpenseTracker/Components/ExpenseList";
-import ExpenseFilter from "./components/ExpenseTracker/Components/ExpenseFilter";
-import ExpenseForm from "./components/ExpenseTracker/Components/ExpenseForm";
+import { useEffect, useRef, useState } from "react";
+import ProductList from "./components/ProductList/ProductList";
 
 function App() {
-    const [selectedCategory, setSelectedCategory] = useState("");
-
-    const [expenses, setExpenses] = useState([
-        { id: 1, description: "aaa", amount: 5, category: "Groceries" },
-        { id: 2, description: "bbb", amount: 5, category: "Utilities" },
-        { id: 3, description: "ccc", amount: 5, category: "Entertainment" },
-        { id: 4, description: "ddd", amount: 5, category: "Entertainment" },
-    ]);
-
-    const visibleExpenses = selectedCategory
-        ? expenses.filter((e) => e.category === selectedCategory)
-        : expenses;
+    const [category, setCategory] = useState("");
+    useEffect(() => {});
 
     return (
         <div>
-            <div className="mb-5">
-                <ExpenseForm
-                    onSubmit={(newExpense) =>
-                        setExpenses([
-                            ...expenses,
-                            { ...newExpense, id: expenses.length++ },
-                        ])
-                    }
-                ></ExpenseForm>
-            </div>
-            <div className="mb-3">
-                <ExpenseFilter
-                    onSelectCategory={(category) =>
-                        setSelectedCategory(category)
-                    }
-                ></ExpenseFilter>
-            </div>
-            <ExpenseList
-                expenses={visibleExpenses}
-                onDelete={(id) =>
-                    setExpenses(expenses.filter((e) => e.id !== id))
-                }
-            ></ExpenseList>
+            <select
+                className="form-select"
+                onChange={(event) => setCategory(event.target.value)}
+            >
+                <option value=""></option>
+                <option value="Clothing">Clothing</option>
+                <option value="Household">Household</option>
+            </select>
+            <ProductList category={category}></ProductList>
         </div>
     );
 }
-
 export default App;
