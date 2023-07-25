84d53328e5c7c27075fd557ed588362b0e4d2a33
sameerkhawaja <sameerkhawaja01@gmail.com>
2023-07-25 14:50:11 -0400

    api demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+    id: number;
+    name: string;
+}
 
 function App() {
-    const connect = () => console.log("Connecting");
-    const disconnect = () => console.log("Disconnecting");
-
+    const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState("");
     useEffect(() => {
-        connect();
-
-        return () => disconnect();
-    });
-
-    return <div></div>;
+        axios
+            .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+            .then((response) => setUsers(response.data))
+            .catch((err) => setError(err.message));
+    }, []);
+    return (
+        <>
+            {error && <p className="text-danger">{error}</p>}
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
 export default App;
